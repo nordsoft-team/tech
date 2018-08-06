8447f1c7548a2a0d6582101d7aa270731f215125
fanfei <chris.aimee.2012@gmail.com>
2018-08-06 19:04:02 +0800

    update

--- tech/src/main/java/shell/vsftpd.sh
+++ tech/src/main/java/shell/vsftpd.sh
@@ -2,4 +2,9 @@
 apt install vsftpd
 mkdir  /srv/ftp/upload
 chmod  777  /srv/ftp/upload
-# cat /etc/vsftpd.conf anon_umask=022
+
+#local_umask=022
+#anon_umask=022
+
+#anon_mkdir_write_enable=YES
+#anon_other_write_enable=YES
