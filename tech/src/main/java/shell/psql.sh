96de5292d39d158f39dce0ba1019501dedb6e436
fanfei <chris.aimee.2012@gmail.com>
2018-08-08 14:01:32 +0800

    update

--- tech/src/main/java/shell/psql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PGPASSWORD="panatripdev"
-function abc(){
-	psql -h 192.168.1.12 -p 5432 -d biqu -U dev<<EOF
-	select * from test_wlh_dom_cache_analysis offset (select count(*) from test_wlh_dom_cache_analysis)-7;
-	\q
-EOF
-}
-abc | grep 2017 | awk 'gsub(/\|/,"\t")';
