986a9a2f7a7ec62bcad85a35a3937ce8ea6f5bdf
fanfei <chris.aimee.2012@gmail.com>
2018-08-10 10:13:06 +0800

    upate

--- tech/src/main/java/shell/gitlab.sh
+++ tech/src/main/java/shell/gitlab.sh
@@ -4,7 +4,9 @@ apt update
 apt-get install -y curl openssh-server ca-certificates
 apt-get install -y postfix
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+apt-get install gitlab-ee
 cd /etc/gitlab
 cp gitlab.rb gitlab.rb.default
-sed -i "s;EXTERNAL_URL="http://gitlab.example.com";EXTERNAL_URL="http://39.106.3.97";" gitlab.rb
+sed -i "s;external_url 'http://gitlab.example.com';external_url 'http://39.106.3.97';" gitlab.rb
 gitlab-ctl reconfigure
+#gitlab-ctl stop
