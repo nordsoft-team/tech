5ef32c7d359fce6d6371da6e96a6738675f745d7
fanfei <chris.aimee.2012@gmail.com>
2018-08-13 10:41:11 +0800

    update

--- tech/src/main/java/shell/git.sh
+++ tech/src/main/java/shell/git.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 apt update
-apt install git
+echo y| apt install git
+cd ~
+git init --bare myRep.git
+
+# CLIENT USAGE
+cd ~
+git clone root@39.106.3.97:/root/myRep.git
+cd ~/myRep
+git pull
+touch abcd.txt
+git add .
+git commit -m 'initial commit'
+git push
