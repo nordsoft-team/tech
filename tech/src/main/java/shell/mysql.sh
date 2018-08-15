63259a0ed59ca8ef1fd50f2e293c98ae233cc5c0
fanfei <chris.aimee.2012@gmail.com>
2018-08-15 12:02:46 +0800

    update

--- tech/src/main/java/shell/mysql.sh
+++ tech/src/main/java/shell/mysql.sh
@@ -2,6 +2,11 @@
 
 apt update
 echo y| apt install mysql-server
-#cd /etc/mysql/mysql.conf.d/
-#sed -i "s;bind-address;#bind-address;" mysqld.cnf
-#/etc/init.d/mysql restart
+
+# update user set host = '%' where user = 'root';
+# grant all privileges on *.* to 'root'@'%' with grant option;
+# flush privileges;
+ 
+# cd /etc/mysql/mysql.conf.d/
+# sed -i "s;bind-address;#bind-address;" mysqld.cnf
+# /etc/init.d/mysql restart
