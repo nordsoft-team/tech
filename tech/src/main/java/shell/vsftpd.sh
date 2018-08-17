558b4b85e0fc10c16cb606ff077635c3f4ab7427
fanfei <chris.aimee.2012@gmail.com>
2018-08-17 19:43:05 +0800

    update

--- tech/src/main/java/shell/vsftpd.sh
+++ tech/src/main/java/shell/vsftpd.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+apt update
 apt install vsftpd
+
+cd /etc
+cp vsftpd.conf vsftpd.conf.default
+
+sed -i "s;anonymous_enable=NO;anonymous_enable=YES;" vsftpd.conf
+
+echo 'local_umask=022' >> vsftpd.conf
+echo 'anon_umask=022' >> vsftpd.conf
+
+echo 'write_enable=YES' >> vsftpd.conf
+echo 'anon_upload_enable=YES' >> vsftpd.conf
+echo 'anon_mkdir_write_enable=YES' >> vsftpd.conf
+echo 'anon_other_write_enable=YES' >> vsftpd.conf
+
+/etc/init.d/vsftpd restart
+
 mkdir  /srv/ftp/upload
 chmod  777  /srv/ftp/upload
-
-#local_umask=022
-#anon_umask=022
-
-#anon_mkdir_write_enable=YES
-#anon_other_write_enable=YES
