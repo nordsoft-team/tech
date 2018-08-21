cf46d217c0d77f68ac66ea9638b77e5106e17ac7
fanfei <chris.aimee.2012@gmail.com>
2018-08-21 10:04:48 +0800

    update

--- tech/src/main/java/shell/nginx.sh
+++ tech/src/main/java/shell/nginx.sh
@@ -3,4 +3,20 @@
 apt update
 echo y| apt install nginx
 
+cd /etc/nginx/
+cp nginx.conf nginx.conf.default
+
+
+#server {
+#        listen			80;
+#        server_name		www.fanfei.tech;
+#
+#        location / {
+#            root		/srv/ftp;
+#            autoindex	on;
+#
+#        }
+#}
+
+
 # /var/www/html
