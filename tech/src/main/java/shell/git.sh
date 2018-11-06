ecb8fc8cf82ba4684a9baa4eee444e7e9ab972ce
樊飞 <fanfei@ctv.org.cn>
2018-11-06 15:14:55 +0800

    update

--- tech/src/main/java/shell/git.sh
+++ tech/src/main/java/shell/git.sh
@@ -3,13 +3,15 @@
 # SERVER USAGE
 apt update
 echo y| apt install git
-cd ~
-git init --bare myRep.git
+adduser git
+sed -i "s;git:x:1000:1000:,,,:/home/git:/bin/bash;git:x:1000:1000:,,,:/home/git:/bin/git-shell;" /etc/passwd
+cd /home/git
+git init --bare yue-demo.git
 
 # CLIENT USAGE
 cd ~
-git clone root@39.106.3.97:/root/myRep.git
-cd ~/myRep
+git clone git@39.106.3.97:/home/git/yue-demo.git
+cd ~/yue-demo
 git pull
 touch abcd.txt
 git add .
