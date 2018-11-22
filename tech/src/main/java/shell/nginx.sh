aee9cd8d3e1753f399c4d5610de55f89b0ab0931
樊飞 <fanfei@ctv.org.cn>
2018-11-22 15:44:38 +0800

    update

--- tech/src/main/java/shell/nginx.sh
+++ tech/src/main/java/shell/nginx.sh
@@ -9,27 +9,27 @@ cp nginx.conf nginx.conf.default
 
 #FTP SERVER CONFIGURATION
 #server {
-#        listen				80;
-#        server_name		www.fanfei.tech;
-#        charset			utf-8;
+#        listen             80;
+#        server_name        www.fanfei.tech;
+#        charset            utf-8;
 #
 #        location / {
-#            root		/srv/ftp;
-#            autoindex	on;
+#            root         /srv/ftp;
+#            autoindex    on;
 #
 #        }
 #}
 
 #HTTPS CONFIGURATION
 #server{
-#        listen					443 default ssl;
-#        ssl 					on;
-#    	ssl_certificate			/etc/nginx/conf.d/www.fanfei.tech.pem;
-#        ssl_certificate_key		/etc/nginx/conf.d/www.fanfei.tech.key;
-#        server_name				www.fanfei.tech;
+#        listen                    443 default ssl;
+#        ssl                       on;
+#        ssl_certificate           /etc/nginx/conf.d/www.fanfei.tech.pem;
+#        ssl_certificate_key       /etc/nginx/conf.d/www.fanfei.tech.key;
+#        server_name               www.fanfei.tech;
 #        location / {
-#	        proxy_redirect		off;
-#			proxy_pass			https://www.taobao.com/;
+#            proxy_redirect        off;
+#            proxy_pass            https://www.taobao.com/;
 #        }        
 #}
 
