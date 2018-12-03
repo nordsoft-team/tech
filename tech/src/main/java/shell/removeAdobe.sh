dcca9a00bab5fc2d0330c73fbba26ab989bb0a9d
樊飞 <fanfei@ctv.org.cn>
2018-12-03 15:42:20 +0800

    update

---
+++ tech/src/main/java/shell/removeAdobe.sh
@@ -0,0 +1,33 @@
+cd /Applications/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd /Applications/Utilities/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd /Library/Application Support/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd /Library/Internet Plug-Ins/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd /Library/LaunchAgents/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd /Library/LaunchDaemons/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd /Library/PrivilegedHelperTools/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd ~/Library/Application Support/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd ~/Library/Containers/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd ~/Library/Group Containers/
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
+cd ~/Library/LaunchAgents
+sudo rm -rf *Adobe*
+sudo rm -rf *adobe*
--- tech/src/main/java/shell/socks_start.sh
+++ tech/src/main/java/shell/socks_start.sh
@@ -7,9 +7,9 @@ echo $txt > abcd.txt
 
 if [[ $1 != "" ]]
 then
-	if [[ $1 -lt "0" || $1 -gt "8" ]]
+	if [[ $1 -lt "0" || $1 -gt "8" || ($1 -gt "2" && $1 -lt "6")]]
 	then
-		echo 'PLEASE INPUT PARAMETER FROM 0 TO 8 OR DO NOT INPUT'
+		echo 'PLEASE INPUT PARAMETER FROM 0 TO 2 OR 6 TO 8 OR DO NOT INPUT'
 		exit
 	else
 		let a=$1
