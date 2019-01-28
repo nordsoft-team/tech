a312dbf8c0ebc17d1b2e842761a3d45bdea5d1e6
樊飞 <fanfei@ctv.org.cn>
2019-01-28 10:56:29 +0800

    update

--- tech/src/main/java/shell/controllerToFeign.sh
+++ tech/src/main/java/shell/controllerToFeign.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-paste -s -d '\0' abcd.txt | tee efgh.txt
-cat efgh.txt |  perl -pe 's/public BaseResponse.*?({.*?;    })/;/g' > abcd.txt
+paste -s -d '\0\ ' abcd.txt | tee efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/public BaseResponse.*?({.*?    })/\1;/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/public class/public interface/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/public class/public interface/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/extends ExceptionHandlerController//g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/@Api//g' > efgh.txt
