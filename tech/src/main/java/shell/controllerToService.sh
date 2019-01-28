efc0dcf306393b895ea7ffb7000b607bd39de87e
樊飞 <fanfei@ctv.org.cn>
2019-01-28 16:18:51 +0800

    update

---
+++ tech/src/main/java/shell/controllerToService.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+if [[ $1 == "" ]]
+then
+	echo 'PLEASE INPUT PARAMETER FILE'
+	exit
+fi
+cat $1 > abcd.txt
+
+paste -s -d ' ' abcd.txt > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/(public BaseResponse.*? )(.*?)(\(.*? )\{(.*?;     )\}/\1\2\3\{return feign.\2;\}/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@.*?Mapping\(.*?\)//g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/extends ExceptionHandlerController//g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@ApiOperation\(.*?\"\)//g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@ApiImplicitParams\({.*?}\)//g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@Api\(.*?\)//g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@Slf4j//g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+
+cat abcd.txt |  perl -pe 's/\@RestController/\@Service/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt ; rm abcd.txt
