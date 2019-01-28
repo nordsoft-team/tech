009b46279aa77f0f3fa169e9fd70a5a91e413d9c
樊飞 <fanfei@ctv.org.cn>
2019-01-28 14:27:09 +0800

    update

--- tech/src/main/java/shell/controllerToFeign.sh
+++ tech/src/main/java/shell/controllerToFeign.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-paste -s -d '\0\ ' abcd.txt | tee efgh.txt
+if [[ $1 == "" ]]
+then
+	echo 'PLEASE INPUT PARAMETER FILE'
+	exit
+fi
+cat $1 > abcd.txt
+
+paste -s -d '\0\ ' abcd.txt > efgh.txt
 cat efgh.txt > abcd.txt ; rm efgh.txt
 
 cat abcd.txt |  perl -pe 's/(public BaseResponse.*?){.*?;     }/\1;/g' > efgh.txt
@@ -17,5 +24,31 @@ cat efgh.txt > abcd.txt ; rm efgh.txt
 cat abcd.txt |  perl -pe 's/\@ApiImplicitParams\({.*?}\)//g' > efgh.txt
 cat efgh.txt > abcd.txt ; rm efgh.txt
 
+cat abcd.txt |  perl -pe 's/\@Api\(.*?\)//g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
 cat abcd.txt |  perl -pe 's/\@Slf4j//g' > efgh.txt
 cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@RestController/\@FeignClient/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@RequestParam\(required = (.*?)\) String (\w+)([,|\)])/\@RequestParam(required = \1, value="\2") String \2\3/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@RequestParam\(required = (.*?)\) Integer (\w+)([,|\)])/\@RequestParam(required = \1, value="\2") Integer \2\3/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@RequestParam\(required = (.*?)\) Long (\w+)([,|\)])/\@RequestParam(required = \1, value="\2") Long \2\3/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@PathVariable\(required = (.*?)\) String (\w+)([,|\)])/\@PathVariable(required = \1, value="\2") String \2\3/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@PathVariable\(required = (.*?)\) Integer (\w+)([,|\)])/\@PathVariable(required = \1, value="\2") Integer \2\3/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt |  perl -pe 's/\@PathVariable\(required = (.*?)\) Long (\w+)([,|\)])/\@PathVariable(required = \1, value="\2") Long \2\3/g' > efgh.txt
+cat efgh.txt > abcd.txt ; rm efgh.txt
+
+cat abcd.txt ; rm abcd.txt
