58216958e572d0e8da2b25679bce8a6deae920fa
樊飞 <fanfei@ctv.org.cn>
2019-01-28 11:04:36 +0800

    update

--- tech/src/main/java/shell/controllerToFeign.sh
+++ tech/src/main/java/shell/controllerToFeign.sh
@@ -2,7 +2,7 @@
 paste -s -d '\0\ ' abcd.txt | tee efgh.txt
 cat efgh.txt > abcd.txt ; rm efgh.txt
 
-cat abcd.txt |  perl -pe 's/public BaseResponse.*?({.*?    })/\1;/g' > efgh.txt
+cat abcd.txt |  perl -pe 's/(public BaseResponse.*?){.*?;    }/\1;/g' > efgh.txt
 cat efgh.txt > abcd.txt ; rm efgh.txt
 
 cat abcd.txt |  perl -pe 's/public class/public interface/g' > efgh.txt
