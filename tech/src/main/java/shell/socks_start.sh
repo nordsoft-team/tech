f8c1fbe15beea665a70ef5d89774b7527a890c35
limiao <limiao@192.168.1.3>
2019-04-04 22:34:55 +0800

    update

--- tech/src/main/java/shell/socks_start.sh
+++ tech/src/main/java/shell/socks_start.sh
@@ -11,7 +11,7 @@ echo $txt > abcd.txt
 
 if [[ $1 == "" || $1 -lt "0" || $1 -gt "8" || ($1 -gt "2" && $1 -lt "6")]]
 then
-	echo 'PLEASE INPUT PARAMETER FROM 0 TO 2 OR 6 TO 8 OR DO NOT INPUT'
+	echo 'PLEASE INPUT PARAMETER FROM 0 TO 2 OR 6 TO 8'
 	exit
 else
 	let a=$1
