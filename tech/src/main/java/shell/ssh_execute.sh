beefe9e76f34f4185c447c3bb30bfe319510e4a7
樊飞 <fanfei@ctv.org.cn>
2019-06-27 16:23:41 +0800

    update

--- tech/src/main/java/shell/ssh_execute.sh
+++ tech/src/main/java/shell/ssh_execute.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 expect << EOF
-spawn ssh root@39.106.3.97
+spawn ssh $1
 
 expect "password:"
-send "Abcd1234\r"
+send "$2\r"
 
 expect "~*"
 send "pwd\r"
@@ -11,4 +11,4 @@ send "pwd\r"
 send  "exit\r"
 expect eof
 EOF
-ls
+pwd
