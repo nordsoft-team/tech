dbaa8f78beb2336c4655c4874792cc084fa2f0dc
樊飞 <fanfei@ctv.org.cn>
2020-03-15 20:23:03 +0800

    update

--- tech/src/main/java/shell/install_homebrew.sh
+++ tech/src/main/java/shell/install_homebrew.sh
@@ -1,9 +1,9 @@
 sudo rm -rf /usr/local/Homebrew
-sudo mkdir /usr/local/Homebrew
+sudo mkdir -p /usr/local/Homebrew
 sudo git clone https://mirrors.ustc.edu.cn/brew.git /usr/local/Homebrew
 
 sudo rm -rf /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
-sudo mkdir /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
+sudo mkdir -p /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 sudo git clone https://mirrors.ustc.edu.cn/homebrew-core.git /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 
 sudo ln -s /usr/local/Homebrew/bin/brew /usr/local/bin/brew
