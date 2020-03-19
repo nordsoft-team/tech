75b4ba612034ab25a35ab490483eb2bb469f8b99
樊飞 <fanfei@ctv.org.cn>
2020-03-20 03:52:30 +0800

    update

--- tech/src/main/java/shell/install_pkg.sh
+++ tech/src/main/java/shell/install_pkg.sh
@@ -1,3 +1,10 @@
 find . -name '*.pkg' | xargs -I{} sudo installer -pkg {} -target /
 
-pkgbuild --identifier com.abcd --root ~/Downloads/src/ --install-location ~/Downloads/dst --scripts ~/Downloads/sh/ ~/Downloads/install.pkg
+pkgbuild --identifier com.abcd --root ~/Downloads/src/ --install-location ~/Downloads/dst/ --scripts ~/Downloads/sh/ ~/Downloads/package.pkg
+
+productbuild --synthesize --package ~/Downloads/package.pkg ~/Downloads/distribution/dist
+#<background file="background"/>
+#<welcome file="welcome"/>
+#<license file="license"/>
+
+productbuild --distribution ~/Downloads/distribution/dist --resources ~/Downloads/resources/ ~/Downloads/product.pkg
