ec15ac608b92b713d3bfd5884b5ad966d89725cd
樊飞 <fanfei@ctv.org.cn>
2020-03-21 23:29:57 +0800

    update

--- tech/src/main/java/shell/remove_office.sh
+++ tech/src/main/java/shell/remove_office.sh
@@ -3,50 +3,22 @@
 function removeFile(){
     if [[ $1 == *microsoft* ]] || [[ $1 == *Microsoft* ]] || [[ $1 == *OneDrive* ]] || [[ $1 == *UBF8T346G9* ]]
     then
-    	echo "$1"
-    	rm -rf "$1"
+    	echo "$1"; rm -rf "$1"
     fi
 }
 
-
-for file in /Applications/*
-do
-	removeFile "$file"
-done
-
-for file in /Library/Application\ Support/*
-do
-	removeFile "$file"
-done
-
-for file in /Library/LaunchDaemons/*
-do
-	removeFile "$file"
-done
-
-for file in /Library/Preferences/*
-do
-	removeFile "$file"
-done
-
-for file in /Library/PrivilegedHelperTools/*
-do
-	removeFile "$file"
-done
-
-for file in ~/Library/Containers/*
-do
-	removeFile "$file"
-done
-
-for file in ~/Library/Group\ Containers/*
-do
-	removeFile "$file"
-done
-
-for file in ~/Library/Preferences/*
-do
-	removeFile "$file"
-done
+for file in /Applications/*; do removeFile "$file"; done;
+for file in /Applications/Utilities/*; do removeFile "$file"; done;
+for file in /Library/Application\ Support/*; do removeFile "$file"; done;
+for file in /Library/Internet\ Plug-Ins/*; do removeFile "$file"; done;
+for file in /Library/LaunchAgents/*; do removeFile "$file"; done;
+for file in /Library/LaunchDaemons/*; do removeFile "$file"; done;
+for file in /Library/Preferences/*; do removeFile "$file"; done;
+for file in /Library/PrivilegedHelperTools/*; do removeFile "$file"; done;
+for file in ~/Library/Application\ Support/*; do removeFile "$file"; done;
+for file in ~/Library/Containers/*; do removeFile "$file"; done;
+for file in ~/Library/Group\ Containers/*; do removeFile "$file"; done;
+for file in ~/Library/LaunchAgents/*; do removeFile "$file"; done;
+for file in ~/Library/Preferences/*; do removeFile "$file"; done;
 
 rm -rf abcd
