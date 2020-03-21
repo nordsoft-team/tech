2c92cb1c70e4583532187b3780d31ee058d7779d
樊飞 <chris.aimee.2012@gmail.com>
2020-03-22 04:24:02 +0800

    update

--- tech/src/main/java/shell/install_pkg.sh
+++ tech/src/main/java/shell/install_pkg.sh
@@ -1,10 +1,10 @@
 find . -name '*.pkg' | xargs -I{} sudo installer -pkg {} -target /
 
-pkgbuild --identifier com.abcd --root ~/Downloads/src/ --install-location ~/Downloads/dst/ --scripts ~/Downloads/sh/ ~/Downloads/package.pkg
+pkgbuild --identifier com.abcd --root ~/Downloads/myPkgs/src/ --install-location ~/Downloads/myPkgs/dst/ --scripts ~/Downloads/myPkgs/scripts/ ~/Downloads/myPkgs/package.pkg
 
-productbuild --synthesize --package ~/Downloads/package.pkg ~/Downloads/distribution/dist
+productbuild --synthesize --package ~/Downloads/myPkgs/package.pkg ~/Downloads/myPkgs/dist
 #<background file="background"/>
 #<welcome file="welcome"/>
 #<license file="license"/>
 
-productbuild --distribution ~/Downloads/distribution/dist --resources ~/Downloads/resources/ ~/Downloads/product.pkg
+productbuild --distribution ~/Downloads/myPkgs/dist --resources ~/Downloads/myPkgs/resources/ ~/Downloads/myPkgs/product.pkg
