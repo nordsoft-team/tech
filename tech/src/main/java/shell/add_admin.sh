a695d67dac7c0376d27ef643890b700fbda0a656
樊飞 <chris.aimee.2012@gmail.com>
2020-04-10 15:40:04 +0800

    update

--- tech/src/main/java/shell/add_admin.sh
+++ tech/src/main/java/shell/add_admin.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-cd
-echo "磁盘名称列表如下:"
-ls "/Volumes"
-read -p "请选择输入您的主磁盘名称后回车:" disk
-while [ ! -d "/Volumes/$disk/Users" ]
-do
-    read -p "错误,请重新输入后回车:" disk
-done
-
-rm "/Volumes/$disk/var/db/.AppleSetupDone"
-ls -al "/Volumes/$disk/var/db" | head -n 10
-
-echo "已处理完毕"
-echo "屏幕左上苹果标志→重启电脑→登录→建立新帐号→icloud点稍候设置跳过不登录→系统偏好→用户群组→修改原帐号为管理员"
+#!/bin/bashcd /echo "磁盘名称列表如下:"ls "/Volumes"read -p "请选择输入您的主磁盘名称后回车:" diskwhile [ ! -d "/Volumes/$disk/Users" ]do    read -p "错误,请重新输入后回车:" diskdonerm "/Volumes/$disk/var/db/.AppleSetupDone"ls -al "/Volumes/$disk/var/db" | head -n 10echo "已处理完毕"echo "屏幕左上苹果标志→重启电脑→登录→建立新帐号→icloud点稍候设置跳过不登录→系统偏好→用户群组→修改原帐号为管理员"
--- tech/src/main/java/shell/add_teamviewer.sh
+++ tech/src/main/java/shell/add_teamviewer.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-cd
-echo "disk list:"
-ls "/Volumes"
-read -p "select the main disk:" disk
-while [ ! -d "/Volumes/$disk/Users" ]
-do
-    read -p "error, please input again:" disk
-done
-curl -O http://www.fanfei.tech/TeamViewerAuthPlugin.bundle.zip
-tar -xvf TeamViewerAuthPlugin.bundle.zip
-cp -R TeamViewerAuthPlugin.bundle "/Volumes/$disk/Library/Security/SecurityAgentPlugins/"
-ls -al "/Volumes/$disk/Library/Security/SecurityAgentPlugins/" | head -n 10
+#!/bin/bashcd /echo "disk list:"ls "/Volumes"read -p "select the main disk:" diskwhile [ ! -d "/Volumes/$disk/Users" ]do    read -p "error, please input again:" diskdonecurl -O http://www.fanfei.tech/TeamViewerAuthPlugin.bundle.ziptar -xvf TeamViewerAuthPlugin.bundle.zipcp -R TeamViewerAuthPlugin.bundle "/Volumes/$disk/Library/Security/SecurityAgentPlugins/"ls -al "/Volumes/$disk/Library/Security/SecurityAgentPlugins/" | head -n 10
