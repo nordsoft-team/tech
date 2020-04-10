6cffc0edc374eec5137bd8faf4f79794c33bef9b
樊飞 <chris.aimee.2012@gmail.com>
2020-04-10 13:54:57 +0800

    update

--- tech/src/main/java/shell/remove_office.sh
+++ tech/src/main/java/shell/remove_office.sh
@@ -15,12 +15,14 @@ for file in /Library/Internet\ Plug-Ins/*; do removeFile "$file"; done;
 for file in /Library/LaunchAgents/*; do removeFile "$file"; done;
 for file in /Library/LaunchDaemons/*; do removeFile "$file"; done;
 for file in /Library/Preferences/*; do removeFile "$file"; done;
+for file in /Library/PreferencePanes/*; do removeFile "$file"; done;
 for file in /Library/PrivilegedHelperTools/*; do removeFile "$file"; done;
 for file in ~/Library/Application\ Support/*; do removeFile "$file"; done;
 for file in ~/Library/Containers/*; do removeFile "$file"; done;
 for file in ~/Library/Group\ Containers/*; do removeFile "$file"; done;
 for file in ~/Library/LaunchAgents/*; do removeFile "$file"; done;
 for file in ~/Library/Preferences/*; do removeFile "$file"; done;
+for file in ~/Library/PreferencePanes/*; do removeFile "$file"; done;
 
 #FORGET FILES
 pkgutil --pkgs / | grep -i "Microsoft" | xargs -I{} sudo pkgutil --forget {}
