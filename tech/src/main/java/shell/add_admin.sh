23adf6ea22606f61c9e1425d1a0f65d479915044
樊飞 <chris.aimee.2012@gmail.com>
2020-04-10 15:58:59 +0800

    update

--- tech/src/main/java/shell/add_admin.sh
+++ tech/src/main/java/shell/add_admin.sh
@@ -3,10 +3,7 @@ cd /
 echo "disk list:"
 ls "/Volumes"
 read -p "select the main disk:" disk
-while [ ! -d "/Volumes/$disk/Users" ]
-do
-    read -p "error, please input again:" disk
-done
+while [ ! -d "/Volumes/$disk/Users" ];do read -p "error, please input again:" disk; done
 
 rm "/Volumes/$disk/var/db/.AppleSetupDone"
 ls -al "/Volumes/$disk/var/db" | head -n 10
--- tech/src/main/java/shell/add_teamviewer.sh
+++ tech/src/main/java/shell/add_teamviewer.sh
@@ -3,10 +3,7 @@ cd /
 echo "disk list:"
 ls "/Volumes"
 read -p "select the main disk:" disk
-while [ ! -d "/Volumes/$disk/Users" ]
-do
-    read -p "error, please input again:" disk
-done
+while [ ! -d "/Volumes/$disk/Users" ];do read -p "error, please input again:" disk; done
 curl -O http://www.fanfei.tech/TeamViewerAuthPlugin.bundle.zip
 tar -xvf TeamViewerAuthPlugin.bundle.zip
 cp -R TeamViewerAuthPlugin.bundle "/Volumes/$disk/Library/Security/SecurityAgentPlugins/"
