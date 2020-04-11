0b1776b02047c19582a39782f3e819aac6fa13df
樊飞 <chris.aimee.2012@gmail.com>
2020-04-11 17:23:57 +0800

    update

--- tech/src/main/java/shell/add_backup.sh
+++ tech/src/main/java/shell/add_backup.sh
@@ -2,23 +2,32 @@
 pwd
 echo "disk list:"
 ls "/Volumes"
-read -p "select the main disk:" disk
+read -p "please select the main disk:" disk
 while [ ! -d "/Volumes/$disk/Users" ];do read -p "error, please input again:" disk; done
 
 echo "disk list:"
 ls "/Volumes"
-read -p "select the usb:" usb
+read -p "please select the usb disk:" usb
 while [ ! -d "/Volumes/$usb" ];do read -p "error, please input again:" usb; done
 
 echo "user list:"
 ls "/Volumes/$disk/Users"
-read -p "select your user:" user
+read -p "please select your user:" user
 while [ ! -d "/Volumes/$disk/Users/$user" ];do read -p "error, please input again:" user; done
 
 
 mkdir -p "/Volumes/$usb/QuickBackup"
 
-echo "copying..."
+echo "copying Desktop..."
+cp -R "/Volumes/$disk/Users/$user/Desktop" "/Volumes/$usb/QuickBackup"
+
+echo "copying Downloads..."
+cp -R "/Volumes/$disk/Users/$user/Downloads" "/Volumes/$usb/QuickBackup"
+
+echo "copying Downloads..."
+cp -R "/Volumes/$disk/Users/$user/Documents" "/Volumes/$usb/QuickBackup"
+
+echo "copying ..."
 cp -R "/Volumes/$disk/Users/$user/Pictures" "/Volumes/$usb/QuickBackup"
 
 echo "done"
