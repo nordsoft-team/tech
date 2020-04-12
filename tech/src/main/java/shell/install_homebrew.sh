ff881eb4b1c72d8e2dd81c344d62efacd7af4433
樊飞 <chris.aimee.2012@gmail.com>
2020-04-12 16:11:52 +0800

    update

--- tech/src/main/java/shell/install_homebrew.sh
+++ tech/src/main/java/shell/install_homebrew.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 sudo rm -rf /usr/local/Homebrew
 sudo mkdir -p /usr/local/Homebrew
-sudo git clone https://mirrors.aliyun.com/homebrew/brew.git /usr/local/Homebrew
+sudo git clone https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git /usr/local/Homebrew
 
 sudo rm -rf /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 sudo mkdir -p /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
-sudo git clone https://mirrors.aliyun.com/homebrew/homebrew-core.git /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
+sudo git clone https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 
 sudo ln -s /usr/local/Homebrew/bin/brew /usr/local/bin/brew
 sudo chown -R $(whoami) /usr/local/Homebrew
 chmod u+w /usr/local/Homebrew
 
-echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.bash_profile
-echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.zshrc
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles' >> ~/.bash_profile
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles' >> ~/.zshrc
 source ~/.bash_profile
 source ~/.zshrc
 
