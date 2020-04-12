06ed0238bd3c68f9c6f91c6963c0ea61271f6c96
樊飞 <chris.aimee.2012@gmail.com>
2020-04-12 15:36:25 +0800

    update

--- tech/src/main/java/shell/add_admin.sh
+++ tech/src/main/java/shell/add_admin.sh
@@ -2,10 +2,11 @@
 pwd
 echo "disk list:"
 ls "/Volumes"
-read -p "select the main disk:" disk
+read -p "please select the main disk:" disk
 while [ ! -d "/Volumes/$disk/Users" ];do read -p "error, please input again:" disk; done
 cd "/Volumes/$disk"
+ls -al "var/db" | head -n 10
 rm "var/db/.AppleSetupDone"
 ls -al "var/db" | head -n 10
 
-echo "over, please don't use icloud"
+echo "done, restart and don't use icloud"
