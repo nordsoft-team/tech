168098561e241adaec15c3aa9404bb263a029e00
樊飞 <chris.aimee.2012@gmail.com>
2020-04-14 11:52:58 +0800

    update

--- tech/src/main/java/shell/install_homebrew.sh
+++ tech/src/main/java/shell/install_homebrew.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo rm -rf /usr/local/Homebrew
 sudo mkdir -p /usr/local/Homebrew
-sudo git clone https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git /usr/local/Homebrew
+sudo git clone https://mirrors.ustc.edu.cn/brew.git /usr/local/Homebrew
 
 sudo rm -rf /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 sudo mkdir -p /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
-sudo git clone https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
+sudo git clone https://github.com/Homebrew/brew.git /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 
 sudo ln -s /usr/local/Homebrew/bin/brew /usr/local/bin/brew
 sudo chown -R $(whoami) /usr/local/Homebrew
@@ -20,17 +20,6 @@ echo "brew doctor"
 brew doctor
 echo "brew update"
 brew update
-
-
-#cd "$(brew --repo)"
-#git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
-#cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-#git remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
-
-#cd "$(brew --repo)"
-#git remote set-url origin https://github.com/Homebrew/brew.git
-#cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-#git remote set-url origin https://github.com/Homebrew/homebrew-core.git
 
 echo "abcd" | pbcopy
 rm -rf abcd
