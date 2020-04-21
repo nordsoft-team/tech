c8038650e8a28b2e45f9ca1745b0a2795467b6a5
樊飞 <chris.aimee.2012@gmail.com>
2020-04-22 00:09:11 +0800

    update

--- tech/src/main/java/shell/remove_anaconda.sh
+++ tech/src/main/java/shell/remove_anaconda.sh
@@ -1,31 +1,31 @@
-#!/bin/bash
+  #!/bin/bash
 
-function removeFile(){
-    if `echo "$1" | grep -qi "anaconda"` || `echo "$1" | grep -qi "continuum"`
-    then
-    	echo "$1"; sudo rm -rf "$1";
-    fi
-}
+  function removeFile(){
+      if `echo "$1" | grep -qi "anaconda"` || `echo "$1" | grep -qi "continuum"`
+      then
+        echo "$1"; sudo rm -rf "$1";
+      fi
+  }
 
-#REMOVE FILES
-for file in /Applications/*; do removeFile "$file"; done;
-for file in /Applications/Utilities/*; do removeFile "$file"; done;
-for file in /Library/Application\ Support/*; do removeFile "$file"; done;
-for file in /Library/Internet\ Plug-Ins/*; do removeFile "$file"; done;
-for file in /Library/LaunchAgents/*; do removeFile "$file"; done;
-for file in /Library/LaunchDaemons/*; do removeFile "$file"; done;
-for file in /Library/Preferences/*; do removeFile "$file"; done;
-for file in /Library/PreferencePanes/*; do removeFile "$file"; done;
-for file in /Library/PrivilegedHelperTools/*; do removeFile "$file"; done;
-for file in ~/Library/Application\ Support/*; do removeFile "$file"; done;
-for file in ~/Library/Containers/*; do removeFile "$file"; done;
-for file in ~/Library/Group\ Containers/*; do removeFile "$file"; done;
-for file in ~/Library/LaunchAgents/*; do removeFile "$file"; done;
-for file in ~/Library/Preferences/*; do removeFile "$file"; done;
-for file in ~/Library/PreferencePanes/*; do removeFile "$file"; done;
+  #REMOVE FILES
+  for file in /Applications/*; do removeFile "$file"; done;
+  for file in /Applications/Utilities/*; do removeFile "$file"; done;
+  for file in /Library/Application\ Support/*; do removeFile "$file"; done;
+  for file in /Library/Internet\ Plug-Ins/*; do removeFile "$file"; done;
+  for file in /Library/LaunchAgents/*; do removeFile "$file"; done;
+  for file in /Library/LaunchDaemons/*; do removeFile "$file"; done;
+  for file in /Library/Preferences/*; do removeFile "$file"; done;
+  for file in /Library/PreferencePanes/*; do removeFile "$file"; done;
+  for file in /Library/PrivilegedHelperTools/*; do removeFile "$file"; done;
+  for file in ~/Library/Application\ Support/*; do removeFile "$file"; done;
+  for file in ~/Library/Containers/*; do removeFile "$file"; done;
+  for file in ~/Library/Group\ Containers/*; do removeFile "$file"; done;
+  for file in ~/Library/LaunchAgents/*; do removeFile "$file"; done;
+  for file in ~/Library/Preferences/*; do removeFile "$file"; done;
+  for file in ~/Library/PreferencePanes/*; do removeFile "$file"; done;
 
-#FORGET FILES
-pkgutil --pkgs / | grep -i "anaconda" | xargs -I{} sudo pkgutil --forget {}
-pkgutil --pkgs / | grep -i "continuum" | xargs -I{} sudo pkgutil --forget {}
+  #FORGET FILES
+  pkgutil --pkgs / | grep -i "anaconda" | xargs -I{} sudo pkgutil --forget {}
+  pkgutil --pkgs / | grep -i "continuum" | xargs -I{} sudo pkgutil --forget {}
 
-rm -rf abcd
+  rm -rf abcd
