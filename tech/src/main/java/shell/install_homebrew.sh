b91a4139f82c043fc422538a683b71fc8cfe59f0
樊飞 <chris.aimee.2012@gmail.com>
2020-04-25 16:40:52 +0800

    update

--- tech/src/main/java/shell/install_homebrew.sh
+++ tech/src/main/java/shell/install_homebrew.sh
@@ -7,6 +7,11 @@ sudo rm -rf /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 sudo mkdir -p /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 sudo git clone https://mirrors.ustc.edu.cn/homebrew-core.git /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
 
+
+#sudo rm -rf /usr/local/Homebrew/Library/Taps/caskroom/homebrew-cask
+#sudo mkdir -p /usr/local/Homebrew/Library/Taps/caskroom/homebrew-cask
+#sudo git clone https://mirrors.ustc.edu.cn/homebrew-cask.git /usr/local/Homebrew/Library/Taps/caskroom/homebrew-cask
+
 sudo rm -rf /usr/local/bin/brew
 sudo ln -s /usr/local/Homebrew/bin/brew /usr/local/bin/brew
 sudo chown -R $(whoami) /usr/local/Homebrew
