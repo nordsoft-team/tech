7d70b7b8fa6735655f166b2a12d5aad249dde124
fanfei <chris.aimee.2012@gmail.com>
2020-05-11 02:17:30 +0800

    add baidu

---
+++ tech/src/main/java/shell/remove_baidu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function removeFile(){
+    if `echo "$1" | grep -qi "BaiduNetdisk"`
+    then
+    	echo "$1"; sudo rm -rf "$1";
+    fi
+}
+
+#REMOVE FILES
+for file in /Applications/*; do removeFile "$file"; done;
+for file in /Applications/Utilities/*; do removeFile "$file"; done;
+for file in /Library/Application\ Support/*; do removeFile "$file"; done;
+for file in /Library/Internet\ Plug-Ins/*; do removeFile "$file"; done;
+for file in /Library/LaunchAgents/*; do removeFile "$file"; done;
+for file in /Library/LaunchDaemons/*; do removeFile "$file"; done;
+for file in /Library/Preferences/*; do removeFile "$file"; done;
+for file in /Library/PreferencePanes/*; do removeFile "$file"; done;
+for file in /Library/PrivilegedHelperTools/*; do removeFile "$file"; done;
+for file in ~/Library/Application\ Support/*; do removeFile "$file"; done;
+for file in ~/Library/Containers/*; do removeFile "$file"; done;
+for file in ~/Library/Group\ Containers/*; do removeFile "$file"; done;
+for file in ~/Library/LaunchAgents/*; do removeFile "$file"; done;
+for file in ~/Library/Preferences/*; do removeFile "$file"; done;
+for file in ~/Library/PreferencePanes/*; do removeFile "$file"; done;
+for file in ~/Library/Caches/*; do removeFile "$file"; done;
+
+#FORGET FILES
+pkgutil --pkgs / | grep -i "BaiduNetdisk" | xargs -I{} sudo pkgutil --forget {}
+
+rm -rf abcd
