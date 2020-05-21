c630432d87d66690c5de86d17cbcd6db68369773
fanfei <chris.aimee.2012@gmail.com>
2020-05-21 16:35:57 +0800

    update

---
+++ tech/src/main/java/shell/move_final.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo scp -r user1@192.168.1.5:"/Users/user1/Movies/Motion\ Templates.localized" "/Users/user2/Movies"
+sudo scp -r user1@192.168.1.5:"/Library/Application\ Support/Final\ Cut\ Pro" "/Library/Application Support/Final Cut Pro"
+sudo scp -r user1@192.168.1.5:"/Library/Application\ Support/Final\ Cut\ Pro\ System\ Support" "/Library/Application Support/Final Cut Pro System Support"
+sudo scp -r user1@192.168.1.5:"/Library/Plug-Ins" "/Library/Plug-Ins"
