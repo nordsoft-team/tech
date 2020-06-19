f6157746b222f549738e5ad8e661372a12285c7d
fanfei <chris.aimee.2012@gmail.com>
2020-06-19 14:53:41 +0800

    update

--- tech/src/main/java/php/index.php
+++ tech/src/main/java/php/index.php
@@ -1,8 +1,10 @@
 <?php
+    // php.ini display_errors = On
     $con = mysqli_connect("localhost","root","123456","mysql");
     $sql = "SELECT now() FROM dual";
     $result = $con->query($sql);
     $row = $result->fetch_assoc();
     echo $row["now()"];
     $con->close();
+    phpinfo();
 ?>
