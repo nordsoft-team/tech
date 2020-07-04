3a78c5fd5cb5395e4f5e05f0ab054c633d2c3695
fanfei <chris.aimee.2012@gmail.com>
2020-07-04 18:36:31 +0800

    update

---
+++ tech/src/main/java/shell/alpine.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/expect
+if {[file exists ~/.ssh/known_hosts]} {
+    file delete [glob ~/.ssh/known_hosts]
+}
+set timeout 5
+set dwp "alpine\r"
+
+spawn ssh -p 2222 root@localhost
+
+expect {
+    "yes/no" {
+        send "yes\n"
+        expect "password:"
+        send "$dwp\n"
+    }
+
+    "*password:*" {
+        send "$dwp\n"
+    }
+}
+expect "*#"
+send "mount -o rw,union,update /\r"
+expect "*#"
+send "cd /var/containers/Data/System/\r"
+expect "*#"
+send "echo hello\r"
+send "dir=\$(find . -name 'internal'| cut -d '/' -f 2)\n cp -rv /var/containers/Data/System/\$dir/Library/activation_records /\r"
+expect "*#"
+spawn scp -r -P 2222 "root@localhost:/var/wireless/Library/Preferences/com.apple.commcenter.device_specific_nobackup.plist"  "/Users/$(whoami)/Desktop/bak/"
+expect {
+    "*password:*" { send $dwp\r;interact }
+}
+
+spawn scp -r -P 2222 "root@localhost:/activation_records" "/Users/$(whoami)/Desktop/bak/"
+expect {
+    "*password:*" { send $dwp\r;interact }
+}
+
+spawn scp -r -P 2222 "root@localhost:/var/mobile/Library/FairPlay/" "/Users/$(whoami)/Desktop/bak/"
+expect {
+    "*password:*" { send $dwp\r;interact }
+}
