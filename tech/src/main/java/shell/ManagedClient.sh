7717ba6990991aaa75022ca35fcd6475431e49c2
fanfei <chris.aimee.2012@gmail.com>
2020-07-11 10:49:49 +0800

    update

--- tech/src/main/java/shell/ManagedClient.sh
+++ tech/src/main/java/shell/ManagedClient.sh
@@ -3,11 +3,11 @@ echo "disk list:"
 ls "/Volumes"
 read -p "please select the main disk:" disk
 while [ ! -d "/Volumes/$disk/Users" ];do read -p "error, please input again:" disk; done
-sudo mkdir "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
-sudo mkdir "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
-sudo mv "/Volumes/$disk/System/Library/LaunchAgents/com.apple.ManagedClientAgent.agent.plist" "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
-sudo mv "/Volumes/$disk/System/Library/LaunchAgents/com.apple.ManagedClientAgent.enrollagent.plist" "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
-sudo mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.cloudconfigurationd.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
-sudo mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.enroll.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
-sudo mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
-sudo mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.startup.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
+mkdir "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
+mkdir "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
+mv "/Volumes/$disk/System/Library/LaunchAgents/com.apple.ManagedClientAgent.agent.plist" "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
+mv "/Volumes/$disk/System/Library/LaunchAgents/com.apple.ManagedClientAgent.enrollagent.plist" "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
+mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.cloudconfigurationd.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
+mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.enroll.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
+mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
+mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.startup.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
