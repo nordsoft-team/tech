2ba2ae50031c5f1406afa03f752a584bcda9e88c
fanfei <chris.aimee.2012@gmail.com>
2020-07-17 23:23:03 +0800

    update

--- tech/src/main/java/shell/alpine.sh
+++ tech/src/main/java/shell/alpine.sh
@@ -24,20 +24,20 @@ expect "*#"
 send "cd /var/containers/Data/System/\r"
 expect "*#"
 send "echo hello\r"
-send "mkdir /Users/$(whoami)/Desktop/bak/"
+send "mkdir -p /Users/admin/Desktop/bak/\r"
 send "dir=\$(find . -name 'internal'| cut -d '/' -f 2)\n cp -rv /var/containers/Data/System/\$dir/Library/activation_records /\r"
 expect "*#"
-spawn scp -r -P 2222 "root@localhost:/var/wireless/Library/Preferences/com.apple.commcenter.device_specific_nobackup.plist"  "/Users/$(whoami)/Desktop/bak/"
+spawn scp -r -P 2222 "root@localhost:/var/wireless/Library/Preferences/com.apple.commcenter.device_specific_nobackup.plist"  "/Users/admin/Desktop/bak/"
 expect {
     "*password:*" { send $dwp\r;interact }
 }
 
-spawn scp -r -P 2222 "root@localhost:/activation_records" "/Users/$(whoami)/Desktop/bak/"
+spawn scp -r -P 2222 "root@localhost:/activation_records" "/Users/admin/Desktop/bak/"
 expect {
     "*password:*" { send $dwp\r;interact }
 }
 
-spawn scp -r -P 2222 "root@localhost:/var/mobile/Library/FairPlay/" "/Users/$(whoami)/Desktop/bak/"
+spawn scp -r -P 2222 "root@localhost:/var/mobile/Library/FairPlay/" "/Users/admin/Desktop/bak/"
 expect {
     "*password:*" { send $dwp\r;interact }
 }
