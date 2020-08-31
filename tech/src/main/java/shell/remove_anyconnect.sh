0187e0b71fc08dfc72d53cb6b3899b7817e1563e
fanfei <chris.aimee.2012@gmail.com>
2020-09-01 00:22:05 +0800

    update

---
+++ tech/src/main/java/shell/remove_anyconnect.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function removeFile(){
+    if `echo "$1" | grep -qi "anyconnect"` || `echo "$1" | grep -qi "cisco"`
+    then
+      echo "$1"; sudo rm -rf "$1";
+    fi
+}
+
+#REMOVE FILES
+for file in /Applications/*; do removeFile "$file"; done;
+for file in /Applications/Utilities/*; do removeFile "$file"; done;
+for file in /Library/Application\ Support/*; do removeFile "$file"; done;
+for file in /Library/Internet\ Plug-Ins/*; do removeFile "$file"; done;
+for file in /Library/LaunchAgents/*; do removeFile "$file"; done;
+for file in /Library/LaunchDaemons/*; do removeFile "$file"; done;
+for file in /Library/Preferences/*; do removeFile "$file"; done;
+for file in /Library/PreferencePanes/*; do removeFile "$file"; done;
+for file in /Library/PrivilegedHelperTools/*; do removeFile "$file"; done;
+for file in ~/Library/Application\ Support/*; do removeFile "$file"; done;
+for file in ~/Library/Containers/*; do removeFile "$file"; done;
+for file in ~/Library/Group\ Containers/*; do removeFile "$file"; done;
+for file in ~/Library/LaunchAgents/*; do removeFile "$file"; done;
+for file in ~/Library/Preferences/*; do removeFile "$file"; done;
+for file in ~/Library/PreferencePanes/*; do removeFile "$file"; done;
+for file in ~/Library/Caches/*; do removeFile "$file"; done;
+
+#FORGET FILES
+pkgutil --pkgs / | grep -i "anyconnect" | xargs -I{} sudo pkgutil --forget {}
+pkgutil --pkgs / | grep -i "cisco" | xargs -I{} sudo pkgutil --forget {}
+
+echo "abcd" | pbcopy
+rm -rf ~/abcd
