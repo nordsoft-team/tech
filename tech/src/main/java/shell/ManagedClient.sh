da756c62916d0f251a435bf164a2e137f8b53882
fanfei <chris.aimee.2012@gmail.com>
2020-09-08 23:37:15 +0800

    update

--- tech/src/main/java/shell/ManagedClient.sh
+++ tech/src/main/java/shell/ManagedClient.sh
@@ -3,11 +3,14 @@ echo "disk list:"
 ls "/Volumes"
 read -p "please select the main disk:" disk
 while [ ! -d "/Volumes/$disk/Users" ];do read -p "error, please input again:" disk; done
-mkdir "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
-mkdir "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
+mkdir -p "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
+mkdir -p "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
 mv "/Volumes/$disk/System/Library/LaunchAgents/com.apple.ManagedClientAgent.agent.plist" "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
 mv "/Volumes/$disk/System/Library/LaunchAgents/com.apple.ManagedClientAgent.enrollagent.plist" "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
 mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.cloudconfigurationd.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
 mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.enroll.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
 mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
 mv "/Volumes/$disk/System/Library/LaunchDaemons/com.apple.ManagedClient.startup.plist" "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
+
+ls -al "/Volumes/$disk/System/Library/LaunchAgentsDisabled";
+ls -al "/Volumes/$disk/System/Library/LaunchDaemonsDisabled";
