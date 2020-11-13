3f805290527540a9a42a7a9bf0f6775458b05949
fanfei <chris.aimee.2012@gmail.com>
2020-11-13 22:35:29 +0800

    update

--- tech/src/main/java/shell/build_product.sh
+++ tech/src/main/java/shell/build_product.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-PRODUCT=myprodocut
+PRODUCT=myproduct
+
+chmod -R 755 ./root
 
 echo "pkgbuild ..."
 pkgbuild  --identifier com.taobao.nordsorft \
-          --scripts ./scripts \
+          --scripts ./darwin/scripts \
           --root ./root \
           ./package/${PRODUCT}.pkg
 
+chmod -R 755 ./package
+
 echo "productbuild ..."
-productbuild  --distribution ./Distribution \
-              --resources ./Resources \
+productbuild  --distribution ./darwin/Distribution \
+              --resources ./darwin/Resources \
               --package-path ./package \
               ./pkg/${PRODUCT}_installer.pkg
