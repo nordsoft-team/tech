b7281b72e4f49d68df20d47250a0893cdbad626d
fanfei <chris.aimee.2012@gmail.com>
2020-11-16 03:05:55 +0800

    update

--- tech/src/main/java/shell/create_catalina.sh
+++ tech/src/main/java/shell/create_catalina.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 sudo pwd
-hdiutil create -o /tmp/Install\ macOS\ Catalina -size 10G -layout SPUD -fs HFS+J
-hdiutil attach /tmp/Install\ macOS\ Catalina.dmg -noverify -mountpoint /Volumes/Install\ macOS\ Catalina
+hdiutil create -size 10240M ~/Desktop/Install\ macOS\ Catalina -ov -layout SPUD -fs HFS+J
+hdiutil attach ~/Desktop/Install\ macOS\ Catalina.dmg -noverify -mountpoint /Volumes/Install\ macOS\ Catalina
 sudo /Applications/Install\ macOS\ Catalina.app/Contents/Resources/createinstallmedia --volume /Volumes/Install\ macOS\ Catalina --nointeraction
 hdiutil detach /Volumes/Install\ macOS\ Catalina
-hdiutil convert -ov /tmp/Install\ macOS\ Catalina.dmg -format UDTO -o ~/Desktop/Install\ macOS\ Catalina
-rm /tmp/Install\ macOS\ Catalina.dmg
+hdiutil convert -ov ~/Desktop/Install\ macOS\ Catalina.dmg -format UDTO -o ~/Desktop/Install\ macOS\ Catalina
