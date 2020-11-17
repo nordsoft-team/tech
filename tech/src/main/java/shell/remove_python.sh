28e7d0fe618d45fe7c87948f7e32de64e64bfce9
fanfei <chris.aimee.2012@gmail.com>
2020-11-17 14:39:56 +0800

    update

--- tech/src/main/java/shell/remove_python.sh
+++ tech/src/main/java/shell/remove_python.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd /Applications
-find . -maxdepth 1 -name "*Python*" |xargs -I{} echo {}
+ls -al |grep "Python"
 find . -maxdepth 1 -name "*Python*" -exec sudo rm -rf {} \;
 
 cd /usr/local/bin
+ls -al |grep "/Library/Frameworks/Python.framework"
 ls -al |grep "/Library/Frameworks/Python.framework" |awk '{print $9}' |xargs -I{} echo {} |xargs -I{} sudo rm -rf {}
 
 echo "/Library/Frameworks/Python.framework"
