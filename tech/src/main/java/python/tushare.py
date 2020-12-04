225bbde73abeb1528db172d25c8e89b42d7d93a2
fanfei <chris.aimee.2012@gmail.com>
2020-12-04 14:08:37 +0800

    update

--- tech/src/main/java/python/tushare.py
+++ tech/src/main/java/python/tushare.py
@@ -5,3 +5,4 @@ import tushare as ts
 pro = ts.pro_api('2de4140def188bc0f9cf85e78b64cf07e58220db85286bbd019edb86')
 df = pro.trade_cal(exchange='', start_date='20180901', end_date='20181001', fields='exchange,cal_date,is_open,pretrade_date', is_open='0')
 df
+print(df)
