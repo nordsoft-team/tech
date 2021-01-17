63e78a7d7a4601ddfc354a724ad3a94ba7370994
fanfei <chris.aimee.2012@gmail.com>
2021-01-17 23:18:04 +0800

    update

--- tech/src/main/java/python/mysql_pthon2.py
+++ tech/src/main/java/python/mysql_pthon2.py
@@ -9,7 +9,7 @@
 import MySQLdb
 
 # 打开数据库连接
-db = MySQLdb.connect("localhost", "root", "", "mysql", charset='utf8' )
+db = MySQLdb.connect("localhost", "root", "", "mysql", charset='utf8')
 
 # 使用cursor()方法获取操作游标
 cursor = db.cursor()
