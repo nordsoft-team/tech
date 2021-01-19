58a069a5d2c310082576622bbaea877f236532b7
fanfei <chris.aimee.2012@gmail.com>
2021-01-19 13:28:41 +0800

    update

--- tech/src/main/java/python/crawler2.py
+++ tech/src/main/java/python/crawler2.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-import os
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
-import time
 import json
+import os
+import requests
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(options=chrome_options)
-driver.get('http://101.200.240.249/site/login')
-
+# 读取cookies
 fr = open('cookies.txt', 'r')
 cookies = json.load(fr)
 fr.close()
 
+# 调用领取按钮请求
+data = {'root_sku_id': '1030'}
+
+cookies1 = {}
 for cookie in cookies:
-    driver.add_cookie(cookie)
+    cookies1[cookie['name']] = cookie['value']
 
-driver.get('http://101.200.240.249/artist/index')
-# 进入首页点击下拉框选择
-selectFaSku = driver.find_element_by_id("selectFaSku")
-
-js = """ele = document.getElementById("selectFaSku").style.display="inline";"""
-driver.execute_script(js)
-Select(selectFaSku).select_by_index(3)
-
-# 领取按钮可用时进行点击
-time.sleep(2)
-receiveButton = driver.find_element_by_class_name("drawDown")
-
-if receiveButton.is_enabled():
-    receiveButton.click()
+res = requests.post(cookies=cookies1, url="http://101.200.240.249/artist/index", data=data)
+print(res.json())
+if res.json().get("status") == 200:
     os.system("say OK")
 
-driver.close()
 print("test2 finished")
