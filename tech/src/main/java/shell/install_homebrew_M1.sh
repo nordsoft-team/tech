982d28cdb1ed3f189536c8e821112874b113c927
fanfei <chris.aimee.2012@gmail.com>
2021-02-13 18:39:54 +0800

    update

--- tech/src/main/java/shell/install_homebrew_M1.sh
+++ tech/src/main/java/shell/install_homebrew_M1.sh
@@ -3,13 +3,15 @@ git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.gi
 /bin/bash -c "$(sed -e 's|^HOMEBREW_BREW_GIT_REMOTE=.*$|HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"|g' -e 's|HOMEBREW_CORE_GIT_REMOTE=.*$|HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"|g' brew-install/install.sh)"
 rm -rf brew-install
 
-grep -qF '/opt/homebrew/bin' /etc/paths || sudo sed -i "" '1i /opt/homebrew/bin' /etc/paths
-grep -qF '/opt/homebrew/share/man' /etc/manpaths || sudo sed -i "" '1i /opt/homebrew/share/man' /etc/manpaths
+grep -qF '/opt/homebrew/bin' /etc/paths || sudo sed -i "" $'1i\\\n/opt/homebrew/bin\n' /etc/paths
+grep -qF '/opt/homebrew/share/man' /etc/manpaths || sudo sed -i "" $'1i\\\n/opt/homebrew/share/man\n' /etc/manpaths
 
 git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
 git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
 
 brew update-reset
+echo 'export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"' >> ~/.zshrc
 
+ohai "Installation successful! Open a new terminal window to use!"
 echo "abcd" | pbcopy
 rm -rf ~/abcd
