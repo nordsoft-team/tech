70d7fa6da706996115bc0780b27a2aacf488d153
fanfei <chris.aimee.2012@gmail.com>
2021-02-23 13:01:19 +0800

    update

--- tech/src/main/java/shell/install_homebrew_M1.sh
+++ tech/src/main/java/shell/install_homebrew_M1.sh
@@ -7,6 +7,9 @@ git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.gi
 /bin/bash -c "$(sed -e 's|^HOMEBREW_BREW_GIT_REMOTE=.*$|HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"|g' -e 's|HOMEBREW_CORE_GIT_REMOTE=.*$|HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"|g' brew-install/install.sh)"
 rm -rf brew-install
 
+git clone https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git /opt/homebrew/Library/Taps/homebrew/homebrew-cask
+git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
+
 grep -qF '/opt/homebrew/bin' /etc/paths || sudo sed -i "" $'1i\\\n/opt/homebrew/bin\n' /etc/paths
 grep -qF '/opt/homebrew/share/man' /etc/manpaths || sudo sed -i "" $'1i\\\n/opt/homebrew/share/man\n' /etc/manpaths
 
