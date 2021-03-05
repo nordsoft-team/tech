e826e16c68832c1a7991d362f79a35ae140f8d0e
fanfei <chris.aimee.2012@gmail.com>
2021-03-05 17:28:46 +0800

    update

--- tech/src/main/java/python/mysql_pthon3.py
+++ tech/src/main/java/python/mysql_pthon3.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 # pip install pymysql
+# conda install pymysql
 
 
 
--- tech/src/main/java/shell/docker.sh
+++ tech/src/main/java/shell/docker.sh
@@ -1,3 +1,13 @@
-docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:8.0.23
-docker run --name jdk -p 8080:8080 -v exam-backend:/root -d openjdk:17-slim java -jar /root/exam-backend.jar
-docker run --name nginx -p 80:80 -v exam-frontend:/usr/share/nginx/html -d nginx:1.18-alpine
+#1.建立exam网络
+docker network create exam
+#2.启动mysql容器
+docker run --name mysql --network exam -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:8.0.23
+docker run --name mysql --network exam -p 3306:3306 -v mysql:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:8.0.23
+
+
+#3.进入容器并建立exam数据库
+docker exec -it mysql /bin/bash
+#4.启动项目容器
+docker run --name jdk --network exam -p 8080:8080 -v jar:/root -d adoptopenjdk:11-jre-hotspot java -jar /root/exam-backend-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
+
+docker run --name nginx -p 80:80 -v angular:/usr/share/nginx/html -d nginx:1.18-alpine
