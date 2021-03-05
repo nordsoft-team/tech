dbdf399a086d2d60b4f8b6ceb9c0751d6112481b
fanfei <chris.aimee.2012@gmail.com>
2021-03-05 22:07:36 +0800

    update

--- tech/src/main/java/shell/docker.sh
+++ tech/src/main/java/shell/docker.sh
@@ -1,10 +1,7 @@
 #1.建立exam网络
 docker network create exam
 #2.启动mysql容器
-docker run --name mysql --network exam -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:8.0.23
 docker run --name mysql --network exam -p 3306:3306 -v mysql:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:8.0.23
-
-
 #3.进入容器并建立exam数据库
 docker exec -it mysql /bin/bash
 #4.启动项目容器
