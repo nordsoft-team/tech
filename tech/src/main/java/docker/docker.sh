b6e85d22bc8786fd938bc81c58627b683898382a
fanfei <chris.aimee.2012@gmail.com>
2021-03-13 00:47:49 +0800

    update

--- tech/src/main/java/docker/docker.sh
+++ tech/src/main/java/docker/docker.sh
@@ -11,16 +11,16 @@ ng build --prod
 docker network create exam
 
 #2.启动mysql容器
-docker run --name mysql --network exam -v mysql:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:8.0.23
+docker run --name mysql --network exam -v mysql:/var/lib/mysql -e TZ=Asia/Shanghai -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:8.0.23
 #3.进入容器并新建exam数据库
-docker exec -it mysql env LANG=C.UTF-8 /bin/bash
+docker exec -it -e LANG=C.UTF-8 mysql /bin/bash
 
 #4.创建jar卷并把本地后端jar复制过去
 docker volume create jar
 #5.启动项目容器
-docker run --name jdk --network exam -v jar:/root -d adoptopenjdk:11-jre-hotspot java -jar /root/exam-backend-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
+docker run --name jdk --network exam -v jar:/root -e TZ=Asia/Shanghai -d adoptopenjdk:11-jre-hotspot java -jar /root/exam-backend-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
 
 #6.启动nginx
-docker run --name nginx --network exam -p 80:80 -p 443:443 -v angular:/usr/share/nginx/html -v nginxConf:/etc/nginx -d nginx:1.18-alpine
+docker run --name nginx --network exam -p 80:80 -p 443:443 -v angular:/usr/share/nginx/html -v nginxConf:/etc/nginx -e TZ=Asia/Shanghai -d nginx:1.18-alpine
 #7.复制前端项目及nginx配置到对应volume然后重启docker
 docker restart nginx
