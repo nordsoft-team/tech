78f22239d60a3cb653489665f29730adcb05ba4b
fanfei <chris.aimee.2012@gmail.com>
2021-04-14 14:56:27 +0800

    update

--- tech/src/main/java/shell/install_homebrew_M1.sh
+++ tech/src/main/java/shell/install_homebrew_M1.sh
@@ -7,6 +7,7 @@ export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.bfsu.edu.cn/git/homebrew/brew.g
 export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.bfsu.edu.cn/git/homebrew/homebrew-core.git"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.bfsu.edu.cn/homebrew-bottles"
 
+rm -rf /opt/homebrew
 git clone --depth=1 https://mirrors.bfsu.edu.cn/git/homebrew/install.git brew-install
 /bin/bash brew-install/install.sh
 rm -rf brew-install
