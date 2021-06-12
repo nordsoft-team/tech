2edccc93e0d68e2654677f0955cd71ad97f06f77
fanfei <chris.aimee.2012@gmail.com>
2021-06-12 19:28:06 +0800

    update

--- tech/src/main/java/shell/parallels.sh
+++ tech/src/main/java/shell/parallels.sh
@@ -1,1 +1,2 @@
+sudo cp /Library/Preferences/Parallels/network.desktop.xml /Library/Preferences/Parallels/network.desktop.xml.default
 sudo sed -i '' 's/>-1</>0</g' /Library/Preferences/Parallels/network.desktop.xml
