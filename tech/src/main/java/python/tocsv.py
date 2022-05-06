986e57bdfcc1328bf6e893ce006c65a466ed1749
fanfei <fanfei@kaolakuaishou.cn>
2022-05-06 16:14:22 +0800

    add tocsv.py

--- tech/src/main/java/python/tocsv.py
+++ tech/src/main/java/python/tocsv.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 # 基于python3.8
 # 将此文件夹包括子文件下的所有excel文件转为csv
-path = '/Users/fanfei/Downloads/202204/'
+path = '/Users/fanfei/Downloads/202204'
 
-print(time.time())
+print(time.asctime())
 for root, dirs, files in os.walk(path):
     for file in files:
         filePath = os.path.join(root, file)
@@ -19,4 +19,4 @@ for root, dirs, files in os.walk(path):
                 if len(excel.items()) > 1:
                     filename = filePath.replace('.xlsx', '', 1) + '_' + key + '.csv'
                 value.to_csv(filename, encoding='utf_8_sig', index=False, header=False)
-print(time.time())
+print(time.asctime())
