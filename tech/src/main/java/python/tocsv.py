cdea4ffa5b1730b180e3d5071225ef4ecc83a5e0
fanfei <fanfei@kaolakuaishou.cn>
2022-05-06 14:58:33 +0800

    add tocsv.py

--- tech/src/main/java/python/tocsv.py
+++ tech/src/main/java/python/tocsv.py
@@ -1,4 +1,5 @@
 import os
+
 import pandas as pd
 
 # 基于python3.8
@@ -11,6 +12,8 @@ for root, dirs, files in os.walk(path):
         if filePath.endswith(".xlsx"):
             print(filePath)
             excel = pd.read_excel(filePath, dtype=str, sheet_name=None)
+            filename = filePath.replace('.xlsx', '.csv', 1)
             for key, value in excel.items():
-                filename = filePath.replace('.xlsx', '', 1) + key + '.csv'
+                if len(excel.items()) > 1:
+                    filename = filePath.replace('.xlsx', '', 1) + key + '.csv'
                 value.to_csv(filename, encoding='utf_8_sig', index=False, header=False)
