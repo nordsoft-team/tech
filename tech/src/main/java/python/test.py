0ad388385798f05cf9e1776e8ec71b8370b1374b
fanfei <fanfei@kaolakuaishou.cn>
2022-05-06 15:22:48 +0800

    add tocsv.py

--- tech/src/main/java/python/test.py
+++ tech/src/main/java/python/test.py
@@ -1,6 +1,8 @@
 import json
+import time
 
 data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ]
 
 json = json.dumps(data)
 print(json)
+print(time.asctime())
--- tech/src/main/java/python/tocsv.py
+++ tech/src/main/java/python/tocsv.py
@@ -1,11 +1,13 @@
 import os
+import time
 
 import pandas as pd
 
 # 基于python3.8
 # 将此文件夹包括子文件下的所有excel文件转为csv
-path = '/Users/fanfei/Downloads/untitled folder 2/untitled folder'
+path = '/Users/fanfei/Downloads/202204/'
 
+print(time.time())
 for root, dirs, files in os.walk(path):
     for file in files:
         filePath = os.path.join(root, file)
@@ -17,3 +19,4 @@ for root, dirs, files in os.walk(path):
                 if len(excel.items()) > 1:
                     filename = filePath.replace('.xlsx', '', 1) + '_' + key + '.csv'
                 value.to_csv(filename, encoding='utf_8_sig', index=False, header=False)
+print(time.time())
