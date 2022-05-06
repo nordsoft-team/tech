90a7ed1d57558ab3d41214e9e2578621c71a80b5
fanfei <fanfei@kaolakuaishou.cn>
2022-05-06 15:00:23 +0800

    add tocsv.py

--- tech/src/main/java/python/tocsv.py
+++ tech/src/main/java/python/tocsv.py
@@ -15,5 +15,5 @@ for root, dirs, files in os.walk(path):
             filename = filePath.replace('.xlsx', '.csv', 1)
             for key, value in excel.items():
                 if len(excel.items()) > 1:
-                    filename = filePath.replace('.xlsx', '', 1) + key + '.csv'
+                    filename = filePath.replace('.xlsx', '', 1) + '_' + key + '.csv'
                 value.to_csv(filename, encoding='utf_8_sig', index=False, header=False)
