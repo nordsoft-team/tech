2a82f2e34ab33c5f4c9aab7458caafc77d888fc1
fanfei <fanfei@kaolakuaishou.cn>
2022-12-01 15:32:59 +0800

    add doris

---
+++ tech/src/main/java/docker/doris.sh
@@ -0,0 +1,41 @@
+# 下载镜像
+docker pull apache/doris:build-env-ldb-toolchain-latest
+
+# 启动FE
+docker run -it -p 8030:8030 -p 9030:9030 -d --name=doris-fe -v /Users/fanfei/Documents/docker/apache-doris-fe-1.1.4-bin:/opt/doris/fe apache/doris:build-env-ldb-toolchain-latest
+
+# 配置FE
+docker exec -it doris-fe /bin/bash
+#/opt/doris/fe/conf/fe.conf priority_networks = 172.17.0.0/16
+
+# 启动FE
+/opt/doris/fe/bin/start_fe.sh --daemon
+
+# 访问FE
+#http://localhost:8030
+
+# 确认模块状态
+show PROC '/backends';
+
+# 启动BE
+docker run -it-d --name=doris-be -v /Users/fanfei/Documents/docker/apache-doris-be-1.1.4-bin-x86_64:/opt/doris/be apache/doris:build-env-ldb-toolchain-latest
+
+# 配置BE
+docker exec -it doris-be /bin/bash
+#/opt/doris/fe/conf/be.conf priority_networks = 172.17.0.0/16
+
+# 启动BE
+/opt/doris/fe/bin/start_be.sh --daemon
+
+# 访问FE
+http://localhost:18030
+
+# 增加节点
+ALTER SYSTEM ADD BACKEND "172.17.0.3:9050";
+
+# 确认模块状态
+show PROC '/backends';
+
+# 通过MYSQL连接
+mysql -h 127.0.0.1 -P9030 -uroot
+# 建库建表确认正常
