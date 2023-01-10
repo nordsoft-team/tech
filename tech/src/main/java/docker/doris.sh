15c318b78deae0b1d704dc9b89f7af201e5d41be
fanfei <fanfei@kaolakuaishou.cn>
2023-01-10 12:02:45 +0800

    modify doris

--- tech/src/main/java/docker/doris.sh
+++ tech/src/main/java/docker/doris.sh
@@ -1,12 +1,12 @@
-# 官网下载二进制文件，保存到如下目录
+# 官网下载二进制文件，保存到如下目录，复制java-udf-jar-with-dependencies.jar到be/lib目录
 # https://doris.apache.org/
-/Users/fanfei/Documents/docker/
+/Users/fanfei/Documents/docker
 
 # 下载镜像
 docker pull apache/doris:build-env-ldb-toolchain-latest
 
 # 启动FE
-docker run -it -p 8030:8030 -p 9030:9030 -d --name=doris-fe -v /Users/fanfei/Documents/docker/apache-doris-fe-1.1.4-bin:/opt/doris/fe apache/doris:build-env-ldb-toolchain-latest
+docker run -it -p 8030:8030 -p 9030:9030 -d --name=doris-fe -v /Users/fanfei/Documents/docker/apache-doris-fe-1.2.1-bin-x86_64:/opt/doris/fe apache/doris:build-env-ldb-toolchain-latest
 
 # 配置FE
 docker exec -it doris-fe /bin/bash
@@ -22,7 +22,7 @@ docker exec -it doris-fe /bin/bash
 show PROC '/frontends';
 
 # 启动BE
-docker run -it -d --name=doris-be -v /Users/fanfei/Documents/docker/apache-doris-be-1.1.4-bin-x86_64:/opt/doris/be apache/doris:build-env-ldb-toolchain-latest
+docker run --privileged -it -d --name=doris-be -v /Users/fanfei/Documents/docker/apache-doris-be-1.2.1-bin-x86_64:/opt/doris/be apache/doris:build-env-ldb-toolchain-latest
 
 # 配置BE
 docker exec -it doris-be /bin/bash
@@ -32,9 +32,9 @@ docker exec -it doris-be /bin/bash
 /opt/doris/be/bin/start_be.sh --daemon
 
 # 访问FE
-http://localhost:18030
+http://localhost:8030
 
-# 增加节点
+# 增加节点，IP为BE的IP
 ALTER SYSTEM ADD BACKEND "172.17.0.3:9050";
 
 # 确认模块alive状态
