316e8b3a5cf66dbbb231bfb80b4aef8eb5495b54
fanfei <fanfei@kaolakuaishou.cn>
2023-11-12 16:51:28 +0800

    angular

--- tech/src/main/java/docker/angular.sh
+++ tech/src/main/java/docker/angular.sh
@@ -1,6 +1,8 @@
 # use brew or use node-v20.9.0.pkg
 brew install node@20
 echo 'export PATH="/usr/local/opt/node@20/bin:$PATH"' >> ~/.zshrc
+sudo chown -R $(whoami) ~/.npm
+sudo chown -R $(whoami) ~/usr/local/lib/node_modules
 
 # install angular
 sudo npm install -g @angular/cli
