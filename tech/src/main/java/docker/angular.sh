e0ac2a63ae1eb45b94e653ec282ac95720a1d633
fanfei <fanfei@kaolakuaishou.cn>
2023-11-12 16:11:52 +0800

    angular

--- tech/src/main/java/docker/angular.sh
+++ tech/src/main/java/docker/angular.sh
@@ -1,4 +1,7 @@
+# use brew or use node-v20.9.0.pkg
 brew install node@20
 echo 'export PATH="/usr/local/opt/node@20/bin:$PATH"' >> ~/.zshrc
-npm install -g @angular/cli
+
+# install angular
+sudo npm install -g @angular/cli
 ng new exam-frontend
