59bf3ad16385008c650a015ed5bd9a1c2d4f870b
fanfei <fanfei@kaolakuaishou.cn>
2024-04-07 11:04:34 +0800

    install mysql by helm

--- tech/src/main/java/docker/helm.sh
+++ tech/src/main/java/docker/helm.sh
@@ -1,5 +1,5 @@
 # uninstall mysql by helm
-helm uninstall test-mysql --namespace=test
+helm uninstall test-mysql -n test
 
 # delete pv and pvc in order to reuse it
 kubectl delete pvc test-mysql-pvc -n test ; kubectl delete pv test-mysql-pv
@@ -8,10 +8,13 @@ kubectl delete pvc test-mysql-pvc -n test ; kubectl delete pv test-mysql-pv
 kubectl apply -f test-mysql-pv-pvc.yaml -n test
 
 # install mysql by helm
-helm install test-mysql bitnami/mysql --namespace=test --set auth.rootPassword=test1234 --set volumePermissions.enabled=true --set primary.persistence.existingClaim=test-mysql-pvc
+helm install test-mysql bitnami/mysql -n test --set auth.rootPassword=test1234 --set volumePermissions.enabled=true --set primary.persistence.existingClaim=test-mysql-pvc
+
+# check if the pod is ready
+kubectl get pods -n test
 
 # get mysql's password
-kubectl get secret --namespace test test-mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d
+kubectl get secret -n test test-mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d
 
 # check if mysql is OK
-kubectl run test-mysql-client --rm --tty -i --restart='Never' --image=mysql:8.0 --namespace=test -- mysql -h test-mysql -ptest1234
+kubectl run test-mysql-client --rm --tty -i --restart='Never' --image=mysql:8.0 -n test -- mysql -h test-mysql -ptest1234
